c959c102f6761e4e3ab2e958a256b724428c12c0
rajrupdasofficial <rajrupofficialid@protonmail.com>
2024-10-15 22:16:01 +0530

    Some additional changes applied
    Signed-off-by: rajrupdasofficial <rajrupofficialid@protonmail.com>

--- enums/target/debug/build/proc-macro2-c5806bffbec14711/out/proc_macro2.d
+++ enums/target/debug/build/proc-macro2-c5806bffbec14711/out/proc_macro2.d
@@ -1,6 +1,6 @@
-/home/laracroft/projects/Rust_projects/rust_tutorials/enums/target/debug/build/proc-macro2-c5806bffbec14711/out/libproc_macro2.rmeta: build/probe.rs
+/home/laracroft/projects/Rust_projects/Rust_tutorial/enums/target/debug/build/proc-macro2-c5806bffbec14711/out/libproc_macro2.rmeta: build/probe.rs
 
-/home/laracroft/projects/Rust_projects/rust_tutorials/enums/target/debug/build/proc-macro2-c5806bffbec14711/out/proc_macro2.d: build/probe.rs
+/home/laracroft/projects/Rust_projects/Rust_tutorial/enums/target/debug/build/proc-macro2-c5806bffbec14711/out/proc_macro2.d: build/probe.rs
 
 build/probe.rs:
 
--- enums/target/debug/deps/enums-3d75d11869be8c64.d
+++ enums/target/debug/deps/enums-3d75d11869be8c64.d
@@ -1,5 +1,5 @@
-/home/laracroft/projects/Rust_projects/rust_tutorials/enums/target/debug/deps/libenums-3d75d11869be8c64.rmeta: src/main.rs
+/home/laracroft/projects/Rust_projects/Rust_tutorial/enums/target/debug/deps/libenums-3d75d11869be8c64.rmeta: src/main.rs
 
-/home/laracroft/projects/Rust_projects/rust_tutorials/enums/target/debug/deps/enums-3d75d11869be8c64.d: src/main.rs
+/home/laracroft/projects/Rust_projects/Rust_tutorial/enums/target/debug/deps/enums-3d75d11869be8c64.d: src/main.rs
 
 src/main.rs:
